388d5cff165bf05c1c71f91f7a2af99b6b9ec6e4
ImbayaDennis <43815591+ImbayaDennis@users.noreply.github.com>
2020-11-28 14:41:19 +0300

    back to fade toggle

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $('.intro').click(() =>{
         $('.tale').delay(450).fadeToggle(300)
-        $('.intro').slideToggle(300);
+        $('.intro').fadeToggle(300);
     });
 
     $('.tale').click(() =>{
         $('.intro').delay(450).fadeToggle(300)
-        $('.tale').slideToggle(300);
+        $('.tale').fadeToggle(300);
     });
 });
