8d82c19e59c26c9c32aa46e43b1bb91233e9c5d2
ImbayaDennis <43815591+ImbayaDennis@users.noreply.github.com>
2020-11-28 14:26:24 +0300

    Slide and fade toggles

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $('.intro').click(() =>{
         $('.tale').fadeToggle(300)
-        $('.intro').fadeToggle(300);
+        $('.intro').slideToggle(300);
     });
 
     $('.tale').click(() =>{
         $('.intro').fadeToggle(300)
-        $('.tale').fadeToggle(300);
+        $('.tale').slideToggle(300);
     });
 });
