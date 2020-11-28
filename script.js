7443bada43eeaf14a4d30edb4d074c9f1fc05030
ImbayaDennis <43815591+ImbayaDennis@users.noreply.github.com>
2020-11-28 14:31:54 +0300

    Delays

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $('.intro').click(() =>{
-        $('.tale').fadeToggle(300)
+        $('.tale').delay(450).fadeToggle(300)
         $('.intro').slideToggle(300);
     });
 
     $('.tale').click(() =>{
-        $('.intro').fadeToggle(300)
+        $('.intro').delay(450).fadeToggle(300)
         $('.tale').slideToggle(300);
     });
 });
